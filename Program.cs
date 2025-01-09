435fe9e62c84831398914be622e448ee1fe46197
jooyb0831 <dia231@naver.com>
2025-01-09 10:26:00 +0900

    정리

--- Program.cs
+++ Program.cs
@@ -5,13 +5,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine(solution(27, 19));
+        Solution sol = new Solution();
+        Console.WriteLine(sol.solution0108(12,23));
     }
 
-        public static int solution(int num1, int num2)
-        {
-        int answer = 0;
-        answer = num1*num2;
-        return answer;
-    }
 }
